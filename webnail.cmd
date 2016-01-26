ae6216f8a421aab2e060361973d689bcb3e3fcba
Bill Zaumen <bill.zaumen+software@gmail.com>
2016-01-26 00:05:56 -0800

    Add webnail.cmd file; update webnail.1 to add --gui option

---
+++ webnail.cmd
@@ -0,0 +1,14 @@
+@echo off
+if not -%1-==-- (
+    javaw -jar WEBNAIL_DIR/webnail.jar
+) else (
+    if %1==--gui  (
+            if not -%2-==-- (
+                javaw -jar WEBNAIL_DIR/webnail.jar %*
+	    ) else (
+	        java -jar WEBNAIL_DIR/webnail.jar %*
+            )
+    ) else (
+        java -jar WEBNAIL_DIR/webnail.jar %*
+    )
+)
