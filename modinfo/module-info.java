915f59656127ed98f7cb202ee0e46ce70ae9744c
Bill Zaumen <bzdev.org@gmail.org>
2022-02-25 16:28:15 -0800

    Added some comments to a source-code file.

--- modinfo/module-info.java
+++ modinfo/module-info.java
@@ -1,4 +1,9 @@
-// used only for webnail server
+// Used only for webnail server - the webnail-server Debian package
+// conflicts with the webnail Debian package because both contain a
+// JAR file for the Java package webnail. The main use of the package
+// webnail-server is inside a Linux container.  For example a Docker
+// image uses webnail-server because it only needs a couple of classes
+// from the full webnail package.
 module webnail {
     exports webnail;
     requires org.bzdev.base;
